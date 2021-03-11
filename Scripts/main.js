8a7cbeddd51a2fd4a83c1b9105c2c43d901cb5c3
Bryson <brysonmg@gmail.com>
2021-03-10 22:33:25 -0600

    Now able to filter entries by mood

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,2 +1,40 @@
+import { useAllEntries } from './JournalData.js';
 import { listEntries } from './JournalList.js';
+import { entryAsHTML } from './JournalEntry.js';
 listEntries();
+
+const mainElement = document.querySelector("main");
+
+mainElement.addEventListener("change", e => {
+    if (e.target.id === "filterMood") {
+
+        let moodSelect = e.target.options.selectedIndex;
+
+        if (e.target.options[moodSelect].value === "all") {
+            const entriesElement = document.querySelector(".entries");
+            entriesElement.innerHTML = filtered2HTML(useAllEntries());
+        } else {
+            showFilteredMoods(e.target.options[moodSelect].innerHTML)
+        }
+    }
+})
+
+
+const filtered2HTML = (arr) => {
+    let newHTML = '';
+    for (const eachObj of arr) {
+        newHTML += entryAsHTML(eachObj)
+    }
+    return newHTML
+}
+
+const showFilteredMoods = (mood) => {
+    const filtered = useAllEntries().filter(eachEntry => {
+        if (eachEntry.mood === mood) {
+            return eachEntry
+        }
+    })
+    
+    const entriesElement = document.querySelector(".entries")
+    entriesElement.innerHTML = filtered2HTML(filtered)
+}
